6c8ae235ad15a51bb856e356ec0934999a46884d
packyzbq <packyzhao@163.com>
2020-11-30 08:54:35 +0800

    fix: 找不到url的错误

--- sakura-vpn/checkin.py
+++ sakura-vpn/checkin.py
@@ -2,14 +2,20 @@ import requests
 import json
 import datetime
 import os
+from bs4 import BeautifulSoup
+
+url_getter="https://poi.aoao.me/"
+r = requests.get(url_getter)
+soup = BeautifulSoup(r.text,'html.parser')
+url = soup.a.text
 
 user=os.environ['SAKURA_USER']
 passwd=os.environ['SAKURA_PASSWD']
 #sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf8') #改变标准输出的默认编码
 payload={'email':user,'passwd':passwd}
-r = requests.post("https://sakura.kozow.com/auth/login",data=payload)
+r = requests.post("https://"+ url +"/auth/login",data=payload)
 if r.json()['ret'] == 1:
-    checkin = requests.post("https://sakura.kozow.com/user/checkin", cookies=r.cookies)
+    checkin = requests.post("https://"+ url +"/user/checkin", cookies=r.cookies)
     if checkin.json()['ret'] == '1':
         print("[",datetime.datetime.now(),"]",checkin.json()['msg'])
     else:
